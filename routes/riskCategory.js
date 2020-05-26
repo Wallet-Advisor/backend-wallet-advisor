f2877e62b1077cc2948d5692fee72dde5fb9bcfe
Ayomide Jones <ayormeday@gmail.com>
2020-05-26 23:14:49 +0100

    creating tables

---
+++ routes/riskCategory.js
@@ -0,0 +1,6 @@
+const { Router } = require('express');
+const {
+    handleRiskCategoryGet
+} = require('controllers/riskCategories/categoryControllers');
+
+// const authenticate = require('middlewares/CategoryValidator');
