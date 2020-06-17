dba51db1c3c5d0fc59eb6993a39885ee5b51ba79
Ayomide Jones <ayormeday@gmail.com>
2020-06-17 15:13:49 +0100

    authentication and category validation middleware imlemented and tested

--- routes/riskCategory.js
+++ routes/riskCategory.js
@@ -3,12 +3,13 @@ const {
     handleCategoriesGet,
     handleAssetsCategories
 } = require('../controllers/riskCategories/categoryControllers');
+const categoryValidator = require('../middlewares/categoryValidator');
 
-// const authenticate = require('middlewares/CategoryValidator');
+const authenticate = require('../api/auth/authenticate');
 
 const router = Router();
 
-router.get('/risk-category',handleCategoriesGet );
-router.get('/assets-category', handleAssetsCategories);
+router.get('/risk-category', authenticate, categoryValidator.categoryScoreValidation, handleCategoriesGet );
+router.get('/assets-category', authenticate, categoryValidator.validateID, handleAssetsCategories);
 
 module.exports = router;
