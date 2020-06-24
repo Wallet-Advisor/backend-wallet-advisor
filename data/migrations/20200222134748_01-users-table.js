9012b8484ffba9013b02e9c1e2fad97a88e52d7a
Ayomide Jones <ayormeday@gmail.com>
2020-06-24 13:54:58 +0100

    formatted db schema in order to push to develop

--- data/migrations/20200222134748_01-users-table.js
+++ data/migrations/20200222134748_01-users-table.js
@@ -1,19 +1,18 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', table => {
-      table.increments();
-      table.string('username').unique();
-      table.string('password').notNullable();
-      table
-        .string('email')
-        .unique()
-        .notNullable();
-      table.string('fullname');
-      table.boolean('admin');
-      table.specificType('profile_image_url', 'text ARRAY');
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('users');
-  };
-  
+  return knex.schema.createTable("users", (table) => {
+    table.increments();
+    table.string("username").unique();
+    table.string("password").notNullable();
+    table
+      .string("email")
+      .unique()
+      .notNullable();
+    table.string("fullname");
+    table.boolean("admin");
+    table.specificType("profile_image_url", "text ARRAY");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
--- data/migrations/20200601104659_02_risk_categories.js
+++ data/migrations/20200601104659_02_risk_categories.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('risk_categories', table => {
-      table.increments();
-      table.integer('score').notNullable();
-      table.string('category').notNullable();
-    });
-  };
-  
+  return knex.schema.createTable("risk_categories", (table) => {
+    table.increments();
+    table.integer("score").notNullable();
+    table.string("category").notNullable();
+  });
+};
+
 exports.down = function(knex) {
-return knex.schema.dropTableIfExists('risk_categories');
+  return knex.schema.dropTableIfExists("risk_categories");
 };
