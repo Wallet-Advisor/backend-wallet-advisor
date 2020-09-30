5091f5ea8f77b684ce9a3073aa0a3c3026d45005
Ayomide Jones <ayormeday@gmail.com>
2020-09-30 16:26:58 +0100

    fixed bugs

--- controllers/userCategoryController.js
+++ controllers/userCategoryController.js
@@ -13,11 +13,17 @@ async function handleUserRiskCategoriesGet(req, res) {
   if(userId){
     try {
       const userCategoryDetails = await db.findInfoByUserId(userId);
+      if(userCategoryDetails.risk_category){
+          const categoryValue = await db.findCategory(userCategoryDetails.risk_category);
+          const userAssets = await db.findByCategoryId(userCategoryDetails.risk_category);
+          userCategoryDetails.asset_category = userAssets;
+          userCategoryDetails.risk_category = categoryValue[0].category;
+      }
       return requestHandler.success(
         res,
         200,
         "User Category details retrieved Successfully",
-        userCategoryDetails
+        userCategoryDetails 
       );
     } catch (error) {
       return requestHandler.error(res, 500, `server error ${error.message}`);
@@ -27,7 +33,6 @@ async function handleUserRiskCategoriesGet(req, res) {
 
 async function handleUserRiskCategoriesPost(req, res) {
     const { userId } = await req.decodedToken;
-    // const foundId = await db.findUserId(id)
     const userInfo = { 
         risk_score: req.body.score,
         risk_category: req.body.category,
---
+++ data/migrations/20200930150802_user_info_category.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("user_info_category", (table) => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable();
+      table.integer("risk_score").notNullable();
+      table.string("risk_category").notNullable();
+      table.integer("asset_category").notNullable();
+      table
+        .foreign("user_id")
+        .references("users.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("user_info_category");
+  };
+  
