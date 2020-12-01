0b38a1936b71435fcb8ec13e84255dc2bea551b3
Ayomide Jones <ayormeday@gmail.com>
2020-12-01 17:20:03 +0100

    rewrite route

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,8 @@ const router = Router();
 
 router.post('/register',UserValidator.userInput, register);
 router.post('/login', UserValidator.userLogin, login);
-router.post('/forgotpassword', UserValidator.inviteInput, passwordReset)
+router.route('/forgotpassword').post(UserValidator.inviteInput, passwordReset);
+// router.post('/forgotpassword', UserValidator.inviteInput, passwordReset)
 router.route('/resetpassword').patch(UserValidator.validateToken, newPassword);
 router.route('/verify_email').post(UserValidator.validateToken, confirmEmail);
 
