6937d1adab995c83aecf4fca242d162bc02da555
Ayomide Jones <ayormeday@gmail.com>
2021-01-05 21:53:12 +0100

    remove searchquery bug

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -1,12 +1,12 @@
-const userModel = require('../../models/userModel');
+const userModel = require("../../models/userModel");
 const requestHandler = require("../../utils/requestHandler");
-const cloudinary = require('../../middlewares/cloudinaryHandler');
+const cloudinary = require("../../middlewares/cloudinaryHandler");
 
 async function handleGetUserList(req, res) {
   try {
     const users = await userModel.getUsers();
-    return requestHandler.success(res, 200, 'Users fetched successfully', {
-      users
+    return requestHandler.success(res, 200, "Users fetched successfully", {
+      users,
     });
   } catch (error) {
     return requestHandler.error(res, 500, `server error ${error.message}`);
@@ -25,8 +25,8 @@ async function handleGetSingleUser(req, res) {
   try {
     const user = await userModel.getSingleUser(searchQuery);
     if (user) {
-      return requestHandler.success(res, 200, 'User fetched successfully', {
-        user
+      return requestHandler.success(res, 200, "User fetched successfully", {
+        user,
       });
     }
     // return requestHandler.error(
@@ -48,17 +48,18 @@ const updateUserProfile = async (req, res) => {
       if (req.file) {
         const currentImage = await JSON.parse(foundUser.profile_image_url);
         req.body.profile_image_url = [
-          { avatar: req.file.secure_url, public_id: req.file.public_id }
+          { avatar: req.file.secure_url, public_id: req.file.public_id },
         ];
         cloudinary.deleteCloudImage(currentImage);
       } else {
-        req.body.profile_image_url = '';
+        req.body.profile_image_url = "";
       }
       const updates = {
         email: req.body.email || foundUser.email,
         // username: req.body.username || foundUser.username,
         fullname: req.body.fullname || foundUser.fullname,
-        profile_image_url: req.body.profile_image_url || foundUser.profile_image_url,
+        profile_image_url:
+          req.body.profile_image_url || foundUser.profile_image_url,
         middle_name: req.body.middle_name || foundUser.middle_name,
         last_name: req.body.last_name || foundUser.last_name,
         address: req.body.address || foundUser.address,
@@ -75,13 +76,16 @@ const updateUserProfile = async (req, res) => {
         company_address: req.body.company_address || foundUser.company_address,
         nationality: req.body.nationality || foundUser.nationality,
         next_of_kin: req.body.next_of_kin || foundUser.next_of_kin,
-        address_of_next_of_kin: req.body.address_of_next_of_kin || foundUser.address_of_next_of_kin,
-        phone_number_of_next_of_kin: req.body.phone_number_of_next_of_kin || foundUser.phone_number_of_next_of_kin,
+        address_of_next_of_kin:
+          req.body.address_of_next_of_kin || foundUser.address_of_next_of_kin,
+        phone_number_of_next_of_kin:
+          req.body.phone_number_of_next_of_kin ||
+          foundUser.phone_number_of_next_of_kin,
         relationship: req.body.relationship || foundUser.relationship,
       };
       const userUpdates = await userModel.updateUser(updates, userId);
-      return requestHandler.success(res, 200, 'Profile updated successfully', {
-        userUpdates
+      return requestHandler.success(res, 200, "Profile updated successfully", {
+        userUpdates,
       });
     }
     return requestHandler.error(res, 400, `You are not authorized to do this`);
@@ -93,5 +97,5 @@ const updateUserProfile = async (req, res) => {
 module.exports = {
   handleGetUserList,
   handleGetSingleUser,
-  updateUserProfile
+  updateUserProfile,
 };
